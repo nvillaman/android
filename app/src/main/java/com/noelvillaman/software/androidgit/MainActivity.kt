55273f6550f37233baadb5bf66d2a2855515f6ff
noelvillaman <namalliv2@hotmail.com>
2020-08-23 15:13:50 -0500

    My first commit

---
+++ app/src/main/java/com/noelvillaman/software/androidgit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.noelvillaman.software.androidgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
