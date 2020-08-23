b3f5bfed510e69e41f0daa06e8afe38776255f4d
noelvillaman <namalliv2@hotmail.com>
2020-08-23 17:07:07 -0500

    Deleted the Toasts that were there for testging

--- app/src/main/java/com/noelvillaman/software/androidgit/MainActivity.kt
+++ app/src/main/java/com/noelvillaman/software/androidgit/MainActivity.kt
@@ -3,6 +3,7 @@ package com.noelvillaman.software.androidgit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,33 +17,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculator(num1 : Int, num2 : Int){
-        if (calulation.text.toString().equals("+")){
-            var sum = num1 + num2
-            showResult.text = "$sum"
-        }
-        else if (calulation.text.toString().equals("x")){
-            var mult = num1 * num2
-            showResult.text = "$mult"
-        }
-        else if (calulation.text.toString().equals("-")){
-            var sub = num1 - num2
-            showResult.text = "$sub"
-        }
-        else if (calulation.text.toString().equals("%")){
-            try {
-                var div = num1/num2
-                showResult.text = "$div"
-            } catch (e : Exception){
-
+        when (calulation.text.toString()) {
+            "+" -> {
+                var sum = num1 + num2
+                showResult.text = "$sum"
             }
+            "-" -> {
+                var sub = num1 - num2
+                showResult.text = "$sub"
+            }
+            "x" -> {
+                var mult = num1 * num2
+                showResult.text = "$mult"
+            }
+            "/" -> {
+                try {
+                    var div = num1/num2
+                    showResult.text = "$div"
+                } catch (e : Exception){
 
+                }
+            }
+            else -> { // Note the block
+                var result = num1 % num2
+                if (result < 0) result + num1 else result
+                showResult.text = "$result"
+            }
         }
-        else {
-            var result = num1 % num2
-            if (result < 0) result + num1 else result
-            showResult.text = "$result"
-        }
-
     }
 
 
